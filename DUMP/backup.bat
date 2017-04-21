974b86ede0e6a739ea624f98a2f3109253c87ed7
robertodeguglielmo <roberto.deguglielmo@gmail.com>
2017-04-21 20:14:05 +0200

    Script d'automatisation des dumps DB

---
+++ DUMP/backup.bat
@@ -0,0 +1,13 @@
+@echo off
+SET mypath=%~dp0
+SET myLogin=root
+rem SET myDatabase=Northwind
+SET myDatabase=locavoitures
+SET myDate=%DATE:~-4%-%DATE:~3,2%-%DATE:~7,2%
+For /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set myTime=%%a-%%b)
+SET myfile=%myDate%_%myTime%
+SET myfileSQL=%mypath%%myfile%_%myDatabase%_DATA_DB.sql
+SET myfileBAT=%mypath%%myfile%_%myDatabase%_RESTORE_DB.bat
+C:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump -h localhost -u %myLogin% -B %myDatabase% -S -H -Y > %myfileSQL%
+echo call backup.bat> %myfileBAT%
+echo C:\wamp\bin\mysql\mysql5.6.17\bin\mysql -u %myLogin% -B %myDatabase% ^< %myfileSQL% >> %myfileBAT%
